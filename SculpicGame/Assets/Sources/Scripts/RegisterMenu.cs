4a46c9993489d2f91474e6546161dc22803574b3
Emilia Szymańska <szymanska.emilka@gmail.com>
2014-11-11 00:20:34 +0100

    Add Register screen. Basic login and registration working.

--- SculpicGame/Assets/Sources/Scripts/RegisterMenu.cs
+++ SculpicGame/Assets/Sources/Scripts/RegisterMenu.cs
@@ -6,30 +6,28 @@ using UnityEngine.UI;
 
 namespace Assets.Sources.Scripts
 {
-    public class LoginMenu : MonoBehaviour
+    public class RegisterMenu : MonoBehaviour
     {
         public InputField LoginField;
         public InputField PasswordField;
-        public void LoginClick()
+        public InputField PasswordField2;
+
+        public void RegisterClick()
         {
             var login = LoginField.text;
             var password = PasswordField.text;
-            if (String.IsNullOrEmpty(login) || String.IsNullOrEmpty(password))
+            var password2 = PasswordField2.text;
+            if (String.IsNullOrEmpty(login) || String.IsNullOrEmpty(password) || String.IsNullOrEmpty(password2) ||
+                password!=password2)
                 return;
-            
             UserService userService = new UserService();
-            var result = userService.LoginUser(login,  SecureString.GetBase64Hash(password)); 
+            var result = userService.AddNewUser(login, SecureString.GetBase64Hash(password));
             Debug.Log(result);
             if (result != null)
             {
                 Player.LogIn(result);
                 Application.LoadLevel("GameScreen");
             }
-        }
-
-        public void RegisterClick()
-        {
-            Application.LoadLevel("RegisterScreen");
         }
     }
 }
