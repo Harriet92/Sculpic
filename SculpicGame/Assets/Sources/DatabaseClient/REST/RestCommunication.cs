06eb5136f4c8f3048dded73226e24a474c3a18d8
Emilia Szymańska <szymanska.emilka@gmail.com>
2014-11-13 00:43:08 +0100

    Further GUI improvement.

--- SculpicGame/Assets/Sources/DatabaseClient/REST/RestCommunication.cs
+++ SculpicGame/Assets/Sources/DatabaseClient/REST/RestCommunication.cs
@@ -1,103 +1,29 @@
-﻿//using System;
-//using System.Collections;
-//using System.Collections.Generic;
-////using HTTP;
-//using HTTP;
+﻿using System.Threading;
+using UnityEngine;
 
-//namespace Assets.Sources.DatabaseServer.REST
-//{//TODO: Add logging
-//    public class RestCommunication
-//    {
-//        private string URL;
-//        private string response;
-//        //private Logging.LoggingHelper loger;
-//        private int retryCount = 0;
-
-//        public RestCommunication()
-//        {
-//            //loger = new Logging.LoggingHelper();
-//        }
-//        public RestCommunication(string postUrl)
-//        {
-//            //loger = new Logging.LoggingHelper();
-//            URL = postUrl;
-//        }
-
-//        public void Send(string json, string password = null)
-//        {
-//            try
-//            {
-//                //loger.WriteLog(String.Format("Start sending Json = {0} to {1}", json, this.URL), Logging.LogLevel.Debug);
-
-//                Hashtable data = new Hashtable();
-//                data.Add("", "");
-//                Request someRequest = new Request("POST", URL);
-
-
-//                someRequest.Text = json;
-//                someRequest.SetHeader("Content-Type", "application/json");
-//                someRequest.SetHeader("charset", "UTF-8");
-//                //AddSessionKeyToHeader(someRequest);
-//                if (!String.IsNullOrEmpty(password))
-//                {
-//                    someRequest.AddHeader("OTPassword-Type", password);
-//                }
-//                someRequest.Send();
-//                int failCounter = 0;
-
-//                while ((!someRequest.isDone || someRequest.response == null) && failCounter < 300)
-//                {
-//                    System.Threading.Thread.Sleep(25);
-//                    failCounter++;
-//                }
-//                if (someRequest.exception != null)
-//                {
-//                    //loger.WriteLog(String.Format("{0} - Communication exception: {1}",URL, someRequest.exception.Message), Logging.LogLevel.Warning);
-//                    throw someRequest.exception;
-//                }
-//                if (someRequest.response == null)
-//                {
-//                    //loger.WriteLog(String.Format("{0} - Response is null",URL), Logging.LogLevel.Warning);
-//                    throw new Exception("Response is null");
-//                }
-
-//                response = someRequest.response.Text;
-//                //SetOtpPassword(someRequest);
-//                //SessionHelper.SetSessionKey(someRequest.response.GetHeader(SessionHelper.ErrorHeaderCode),
-//                //someRequest.response.GetHeader(SessionHelper.SessionHeaderCode));
-
-//                //loger.WriteLog(String.Format("Successful send to {0}", this.URL), Logging.LogLevel.Info);
-//            }
-//            catch (Exception)
-//            {
-//                if (retryCount <= 3)
-//                {
-//                    //loger.WriteLog(String.Format("{0} - Retry request",URL), Logging.LogLevel.Warning);
-//                    retryCount++;
-//                    Send(json, password);
-//                }
-//                //loger.WriteLog(String.Format("{0} - COMMUNICATION ERROR!", URL), Logging.LogLevel.Error);
-//            }
-//        }
-
-
-//        public string Recive()
-//        {
-//            //loger.WriteLog(String.Format("Successful recive from {0}", URL), Logging.LogLevel.Info);
-//            //loger.WriteLog(String.Format("Recive: {0} from {1}", response, URL), Logging.LogLevel.Debug);
-//            return response;
-//        }
-
-//        public string SendAndRecive(string json, string password)
-//        {
-//            Send(json, password);
-//            return Recive();;
-//        }
-
-//        public void SetUrl(string url)
-//        {
-//            URL = url;
-//            //loger.WriteLog("SET URL = " + this.URL + " in [WebPlayerRestCommunication]", Logging.LogLevel.Debug);
-//        }
-//    }
-//}
+namespace Assets.Sources.DatabaseClient.REST
+{//TODO: Add logging
+    public class RestCommunication
+    {
+        private const bool isLocalhost = false;
+        private const int retryCount = 5;
+        private const int sleepTimeout = 500;
+        public const string BaseURL = isLocalhost ? "http://localhost:8733" : "http://deemi.ddns.net:8733";
+        public string SendAndReceive(string url)
+        {
+            WWW www = new WWW(url);
+            //Thread thread = new Thread(() =>
+            //{
+                Debug.Log("Sending:" + www.url);
+                int noOfRetry = 0;
+                while (!www.isDone && noOfRetry++ < retryCount)
+                {
+                    Thread.Sleep(sleepTimeout);
+                }
+            //});
+            //thread.Start();
+            //thread.Join();
+            return www.isDone ? www.text : null;
+        }
+    }
+}
--- SculpicGame/Assets/Sources/Scripts/LoginMenu.cs
+++ SculpicGame/Assets/Sources/Scripts/LoginMenu.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using Assets.Sources.DatabaseClient.Security;
 using Assets.Sources.DatabaseClient.Services;
 using UnityEngine;
@@ -10,26 +11,53 @@ namespace Assets.Sources.Scripts
     {
         public InputField LoginField;
         public InputField PasswordField;
+        public Canvas IncorrectLoginCanvas;
+
+        public Image LoadingIndicator;
+        public Text LoginEmpty;
+        public Text PassEmpty;
+        private Color color;
+        void Start()
+        {
+            color = LoginField.GetComponentInChildren<Text>().color;
+            LoginField.GetComponentInChildren<Text>().color = new Color(131, 1, 1, 0);
+            PasswordField.GetComponentInChildren<Text>().color = new Color(131, 1, 1, 0);
+        }
         public void LoginClick()
         {
             var login = LoginField.text;
             var password = PasswordField.text;
             if (String.IsNullOrEmpty(login) || String.IsNullOrEmpty(password))
+            {
+                if (String.IsNullOrEmpty(login))
+                    LoginField.GetComponentInChildren<Text>().color = color;
+
+                if (String.IsNullOrEmpty(password))
+                    PasswordField.GetComponentInChildren<Text>().color = color;
                 return;
-            
+            }
             UserService userService = new UserService();
-            var result = userService.LoginUser(login,  SecureString.GetBase64Hash(password)); 
+            var result = userService.LoginUser(login, SecureString.GetBase64Hash(password));
             Debug.Log(result);
             if (result != null)
             {
                 Player.LogIn(result);
                 Application.LoadLevel("GameScreen");
             }
+            else
+            {
+                Instantiate(IncorrectLoginCanvas);
+            }
         }
 
         public void RegisterClick()
         {
             Application.LoadLevel("RegisterScreen");
+        }
+
+        public void IncorrectInputButtonClick()
+        {
+            IncorrectLoginCanvas.gameObject.SetActive(false);
         }
     }
 }
