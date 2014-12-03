e3a4520357cc073b225add800b5c998784d3885f
Emilia Szymańska <szymanska.emilka@gmail.com>
2014-12-04 00:13:53 +0100

    DrawerScreen template finished (with basic chat handling).

---
+++ SculpicGame/Assets/Sources/Scripts/GuesserScreen/GuesserGUI.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Assets.Sources.Common;
+using Assets.Sources.Scripts.Sculptor;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Sources.Scripts.DrawerScreen
+{
+    public class GuesserGUI : MenuBase
+    {
+        public Text ChatTextField;
+        public InputField ChatInputField;
+        public void OnTextFieldEditEnd(string value)
+        {
+            StringBuilder builder = new StringBuilder(ChatTextField.text);
+            ChatInputField.text = String.Empty;
+            ChatTextField.text = builder.AppendLine(CreateUserMessageLine(value)).ToString();
+        }
+
+        private string CreateUserMessageLine(string value)
+        {
+            return (Player.Current == null ? "Stranger: " : Player.Current.Username + " :") + value;
+        }
+
+    }
+}
