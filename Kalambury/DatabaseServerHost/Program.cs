e46e9e560f0f60889ab2a6233a7a74d31def6fd7
Emilia Szymańska <szymanska.emilka@gmail.com>
2014-12-14 18:45:08 +0100

    WCF Self - hosted console application created.

---
+++ Kalambury/DatabaseServerHost/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace DatabaseServerHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost svcHost = null;
+            try
+            {
+                svcHost = new ServiceHost(typeof(Kalambury.WcfServer.Services.UserService));
+                svcHost.Open(); 
+                Console.WriteLine("Server started at ");
+                Console.WriteLine("\nlocalhost:8733/UserService");
+            }
+            catch (Exception eX)
+            {
+                svcHost = null;
+                Console.WriteLine("Service can not be started \n\nError Message [" + eX.Message + "]");
+            }
+            if (svcHost == null) return;
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+            svcHost.Close();
+        }
+    }
+}
