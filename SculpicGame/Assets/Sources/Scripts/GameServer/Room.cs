1fc011d68e184b2311e9557902c563998b0ee118
Nahikka <dominiska@gmail.com>
2015-01-06 00:38:39 +0100

    Added drawers queue.

--- SculpicGame/Assets/Sources/Scripts/GameServer/Room.cs
+++ SculpicGame/Assets/Sources/Scripts/GameServer/Room.cs
@@ -1,12 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets.Sources.Scripts.GameServer
 {
-    class Room
+    [RequireComponent(typeof(NetworkView))]
+    class Room : MonoBehaviour
     {
+        // RoomOwner
+        private readonly List<NetworkPlayer> _drawers = new List<NetworkPlayer>();
+
+        [RPC]
+        public void SignUpForDrawing(NetworkPlayer player)
+        {
+            Debug.Log("Method RoomOwner.SignUpForDrawing");
+            _drawers.Add(player);
+            Debug.Log("Drawers count: " + _drawers.Count);
+        }
+
+        [RPC]
+        public void SignOffFromDrawing(NetworkPlayer player)
+        {
+            Debug.Log("Method RoomOwner.SignOffFromDrawing");
+            if (_drawers.Remove(player))
+                Debug.Log("Removed player from drawing queue.");
+            Debug.Log("Drawers count: " + _drawers.Count);
+        }
+
+        public void OnWantToDrawValueChanged(Toggle callingObject)
+        {
+            bool wantToDraw = callingObject.isOn;
+            Debug.Log("Method RoomOwner.WantToDrawToggle: wantToDraw == " + wantToDraw);
+            networkView.RPC(wantToDraw ? "SignUpForDrawing" : "SignOffFromDrawing", RPCMode.Server, Network.player);
+        }
+
+        // TODO: checking if phrase matches
+        // TODO: assigning drawer and phrase
+
+
         // TODO: player dictionary
 
         // TODO: recieving data (winner, points, next drawer)
