a4142ddb1439844d4fe84ca69234ba40f5897f78
Emilia Szymańska <szymanska.emilka@gmail.com>
2015-01-22 14:21:16 +0100

    Scroll view on Ranking Screen fixed, ranking button added to RoomChoiceScreen, some visual adjustments.

---
+++ SculpicGame/Assets/Sources/Scripts/RankingScreen/RankingButton.cs
@@ -0,0 +1,13 @@
+﻿using Assets.Sources.Enums;
+using UnityEngine;
+
+namespace Assets.Sources.Scripts.RankingScreen
+{
+    public class RankingButton : MonoBehaviour
+    {
+        public void OnClick()
+        {
+            Application.LoadLevel(SceneName.RankingScreen.ToString());
+        }
+    }
+}
